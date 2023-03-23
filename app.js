1e1b9dc11b44d06fa476d241b3c39cfacba46f9f
Djulinda <126629987+DjuliCoelho@users.noreply.github.com>
2023-03-23 11:20:30 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,46 +1,17 @@
-"use strict";
-/*let valor: number;
-import { calcQuadrado } from './Func';
-let i: number;
+const express = require("express");
+const servidor = express();
 
-valor = 8;
+servidor.get("/", function(req, res){
+    res.send("Seja bem viado aqui na minha aplicação!");
 
-if (valor >= 10){
-    console.log("É maior ou igual a 10!");
-}else{
-    if(valor < 2){
-        console.log("O valor é menor que 2!");
-    }else{
-        console.log("Número ficou entre 2 e 9");
-    }
-}
+});
 
-for (i = 0; i < 10; i++){
-    console.log("O valor do contador é %d" , i);
-}
+servidor.get("/login",function(req,res){
+    res.send("Esta é a página de login da aplicação");
 
-i = 0;
-while(i < 10){
-    console.log("O valor do contador é %d" , i);
-    i++;
+});
+ 
+servidor.listen(8085, function(){
+    console.log("Servidor rodando,chuchu");
 
-}
-
-i = 0;
-do{
-    console.log("O valor do contador é %d" , i);
-    i++;
-}while(i < 10);*/
-exports.__esModule = true;
-var Func_1 = require("./Func");
-var x, y;
-var teclado = require("prompt-sync")();
-//let result: number;
-console.log("Informe primeiro  valor: ");
-x = teclado();
-console.log("Informe o segundo valor: ");
-y = teclado();
-x = Number(x);
-y = Number(y);
-//result = calcQuadrado(5);
-console.log((0, Func_1.calcSoma)(x, y));
+});
